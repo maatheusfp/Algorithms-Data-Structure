ca7df1523ff378083a2dd91a43991e154417be71
Matheus Pinheiro <matheus.pinheirofer@gmail.com>
2023-07-12 20:29:40 -0300

    initialized stack list

--- lists/Stacks/stack.cpp
+++ lists/Stacks/stack.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 typedef struct link{
@@ -15,5 +16,65 @@ Link* create_link(int it, Link* nextval){
     Link* n = (Link*) malloc(sizeof(Link));
     n->element = it;
     n->next = nextval;
-    retun n;
+    return n;
+}
+
+Stack* create_stack(){
+    Stack* s = (Stack*) malloc(sizeof(Stack));
+    s->top = NULL;
+    s->size = 0;
+    return s;
+}
+
+void push(Stack* s, int it){
+    s->top = create_link(it, s->top); // add E it to the top 
+    s->size++;
+}
+
+int pop(Stack* s){
+    if (s->top == NULL){
+        cout << "error" << endl;
+    }
+    else {
+        int it = s->top->element;
+        s->top = s->top->next;
+        s->size--;
+        return it;
+    }
+}
+
+int sum(Stack* s, int num){
+    int count = 0;
+    int aux;
+    while(num--){
+        aux = pop(s);
+        count += aux;
+    }
+    return count;
+}
+
+int main(){
+    int c, numero, soma;
+    string comando;
+    cin >> c;
+
+    for (int i = 1;i <= c;i++){
+        cout << "Caso" << " " << i << ":" << endl;
+
+        while (comando != "end"){
+            cin >> comando >> numero;
+
+            Stack* s = create_stack();
+
+            if (comando == "push"){
+                push(s, numero);
+            }
+            else if (comando == "pop"){
+                soma = sum(s, numero);
+                cout << soma << endl;
+            }
+        }   
+    }
+    
+    return 0;
 }

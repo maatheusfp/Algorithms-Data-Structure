1157f8f62513637f0154a91e58cb0ae5ba2d3498
mfp2 <mfp2@g4c11.cin.ufpe.br>
2023-07-19 12:22:15 -0300

    Almost running

--- Hashing/ClosedHashing/intCH.cpp
+++ Hashing/ClosedHashing/intCH.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string.h>
+
 using namespace std;
 
 int h(int k){
@@ -7,26 +9,63 @@ int h(int k){
 }
 
 typedef struct entry{
-    int* k;
-    int v;
+    long long int CPF;
+    string nome;
 } Entry;
 
 typedef struct hashDict {
     int m, cnt; 
     Entry* H;
-    int* Perm;
+    int Perm[9] = {2, 5, 7, 8, 9, 1, 3, 4, 6};
 } HashDict;
 
-int* create_permutation(){
+int find(HashDict* d, long long int CPF){
+    int pos = h(CPF);
+    int newPos = pos;
+    int i = 0;
+    int contador = 0;
 
+    do {
+        if (d->H[newPos].CPF == CPF){
+            return newPos;
+        }
+        i++;
+        int offset = d->Perm[i - 1];
+        newPos = (pos + offset)%d->m;
+        contador++;
+    } while (contador <= 10);
+
+    return -1;
+}
+
+void insert(HashDict* d, long long int CPF, string nome){
+    if(d->cnt >= d->m || find(d, CPF) != -1){
+        return;
+    }
+
+    int pos = h(CPF);
+    int newPos;
+    if (d->H[pos].CPF != 0){
+        int i = 0;
+        do {
+            i++;
+            int offset = d->Perm[i - 1];
+            newPos = (pos + offset)%d->m;
+        } while (d->H[newPos].CPF != 0);
+        pos = newPos;
+    }
+    Entry entry;
+    entry.CPF = CPF;
+    entry.nome = nome;
+    d->H[pos] = entry;
+    d->cnt++;
 }
 
 HashDict* create_dict(int size){
     HashDict* d = (HashDict*) malloc(sizeof(HashDict));
     d->m = size;
     d->cnt = 0;
-    d-> h = new Entry[size];
-    d->Perm = create_permutation()
+    d->H = new Entry[size];
     return d;
 }
 

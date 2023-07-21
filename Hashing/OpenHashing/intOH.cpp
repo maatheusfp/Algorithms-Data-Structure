3fdd2b0be3a34fe7ccb46c7c445943641dde709d
Matheus Pinheiro <matheus.pinheirofer@gmail.com>
2023-07-21 11:49:52 -0300

    working in Open Hashing

--- Hashing/OpenHashing/intOH.cpp
+++ Hashing/OpenHashing/intOH.cpp
@@ -6,27 +6,54 @@ typedef struct link {
     link* next;
 } Link;
 
-typedef struct list {
-    Link* head;
-    Link* tail;
-    Link* curr;
-    int cnt;
-} List;
+typedef struct stack{
+    Link* top;
+    int size;
+} Stack; 
 
-typedef struct dict {
-    int m;
-    int cnt;
-    List* H;
-    //  h : Key -> 0 
+typedef struct dict{
+    int m, cnt;
+    Stack* H;
 } Dict;
 
-
-
-int h(int K){
-    // create the hash function in here depending on the mecanism
+Link* create_link(int it, Link* nextval){
+    Link* n = (Link*) malloc(Link);
+    n->element = it;
+    n->next = nextval;
+    return n;
 }
 
+Stack* create_stack(){
+    Stack* s = (Stack*) malloc(Stack);
+    s->top = NULL;
+    s->size = 0;
+    return l;
+}
 
+int h(int k){
+    return abs(k) % 10;
+}
+
+int find(Dict* d, int value){
+    int pos = h(value);
+    
+}
+
+void append(Stack* s, int it){
+    s->top = create_link(it, l->top);
+    s->size;
+}
+
+Dict* create_dict(int size){
+    Dict* d = (Dict*) malloc(Dict);
+    d->m = size;
+    d->cnt = 0;
+    d->H = new Stack[size];
+    for (int i = 0; i < size; i++){
+        d->H[i] = create_stack();
+    }
+    return d;
+}
 
 int main(){
 

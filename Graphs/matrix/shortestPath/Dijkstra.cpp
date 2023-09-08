2e20c7097ac1083e50f83ab3f5593eee8f53d5f6
Matheus Pinheiro <matheus.pinheirofer@gmail.com>
2023-09-08 01:46:14 -0300

    dijkstra working

--- Graphs/matrix/shortestPath/Dijkstra.cpp
+++ Graphs/matrix/shortestPath/Dijkstra.cpp
@@ -2,14 +2,14 @@
 #define pp pair<int,int>
 using namespace std;
 
-long long int Dijkstra(int start, int n, vector<pp> graph){
+long long int Dijkstra(int start, int destiny, int n, vector<pp> graph[]){
     vector <long long int> dist(n, 500000001);
     priority_queue<pp, vector<pp>, greater<pp>> heap;
     dist[start] = 0;
     heap.push(make_pair(dist[start], start));
 
     while(!heap.empty()){
-        int u = heap.top().second; // aresta
+        int u = heap.top().second; // vértice
         long long int d = heap.top().first; // distancia
         heap.pop();
 
@@ -19,12 +19,34 @@ long long int Dijkstra(int start, int n, vector<pp> graph){
             int w = edge.first;
             int v = edge.second;
 
-            if (dist[v] > dist[u] + w)
+            if (dist[v] > dist[u] + w){
+                dist[v] = dist[u] + w;
+                heap.push(make_pair(dist[v], v));
+            }
         }
     }
+    return dist[destiny];
 }
 
-
 int main(){
+    int cases, n, m, s, t, u, v, w;
+    cin >> cases;
+    
+    for (int i = 1; i <= cases; i++){
+        cin >> n >> m >> s >> t;
+        vector<pp> graph[n];
+        while (m--){
+            cin >> u >> v >> w;
+            graph[u].push_back(make_pair(w, v));
+            graph[v].push_back(make_pair(w, u)); // nao direcionado
+        }
+        long long int result = Dijkstra(s, t, n, graph);
+        if (result < 500000001){
+            cout << "Case" << " " << "#" << i << ":" << " " << result << endl;
+        }
+        else {
+            cout << "Case" << " " << "#" << i << ":" << " " << "unreachable" << endl;
+        }
+    }
     return 0;
 }
